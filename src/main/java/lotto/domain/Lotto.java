37c6fd3ae4d4a15d962a7c4608a6491a69ce6e06
usingnameing <cnrrn0312@gmail.com>
2025-10-31 18:09:04 +0900

    feat(Lotto): add Lotto class

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        Collections.sort(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        if(InputValidator.validateDuplicateNumbers(numbers)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+        }
+
+        if(InputValidator.validateInvalidNumbers(numbers)){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45사이의 번호여야 합니다.");
+        }
+    }
+
+    // TODO: 추가 기능 구현
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(this.numbers);
+    }
+}
