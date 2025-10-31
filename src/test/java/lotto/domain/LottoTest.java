bc8b35ff76adaad47ae8547147784b419f3deea9
usingnameing <cnrrn0312@gmail.com>
2025-10-31 18:20:37 +0900

    test(domain): add Lotto duplicate number and invalidate number test

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -23,4 +23,11 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
+    @DisplayName("로또 번호가 1에서 45 사이의 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_범위밖의_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(()-> new Lotto(List.of(1,2,3,4,5,67)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
